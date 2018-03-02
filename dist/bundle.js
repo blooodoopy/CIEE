7bdfc57a9a478d2e8f217bd60f2c09991115a40c
Jediel Ribeiro <blooo_doopy@hotmail.com>
2018-03-02 14:27:08 -0300

    Finished project

--- dist/bundle.js
+++ dist/bundle.js
@@ -132,6 +132,22 @@ const produtos = [
   },
 
   {
+    nome: 'Colgate',
+    tipo: 'pasta de dente',
+    preço: 2.99,
+    validade: {
+      dia: 14,
+      mês: 10,
+      ano: 2018
+    },
+    chegada: {
+      dia: 1,
+      mês: 3,
+      ano: 2019
+    }
+  },
+
+  {
     nome: 'Coca cola',
     tipo: 'bebida',
     preço: 5.49,
@@ -202,7 +218,39 @@ const produtos = [
     garantia: {
       dia: 1,
       mês: 3,
-      ano: 2029
+      ano: 2019
+    },
+    chegada: {
+      dia: 1,
+      mês: 3,
+      ano: 2018
+    }
+  },
+
+  {
+    nome: 'pizza',
+    tipo: 'comida',
+    preço: 59.99,
+    validade: {
+      dia: 1,
+      mês: 3,
+      ano: 2019
+    },
+    chegada: {
+      dia: 1,
+      mês: 3,
+      ano: 2018
+    }
+  },
+
+  {
+    nome: 'café',
+    tipo: 'bebida',
+    preço: 8.99,
+    validade: {
+      dia: 1,
+      mês: 3,
+      ano: 2020
     },
     chegada: {
       dia: 1,
@@ -210,6 +258,7 @@ const produtos = [
       ano: 2018
     }
   }
+
 ]
 
 for(let i = 0; i <produtos.length; i++) {
@@ -220,14 +269,51 @@ for(let i = 0; i <produtos.length; i++) {
 const textInput = document.getElementById('textSearch')
 const itemFound = document.getElementById('itemContainer')
 
+const nome = document.getElementById('nome')
+const tipo = document.getElementById('tipo')
+const preço = document.getElementById('preço')
+const diaVal = document.getElementById('diaVal')
+const mesVal = document.getElementById('mesVal')
+const anoVal = document.getElementById('anoVal')
+const diaChegada = document.getElementById('diaChegada')
+const mesChegada = document.getElementById('mesChegada')
+const anoChegada = document.getElementById('anoChegada')
+const responsavel = document.getElementById('responsavel')
+
+const button = document.getElementById('submit')
+
+button.addEventListener('click', () => {
+  let newProduto = {
+    nome: nome.value,
+    tipo: tipo.value,
+    preço: preço.value,
+    validade: {
+      dia: diaVal.value,
+      mes: mesVal.value,
+      ano: anoVal.value
+    },
+    chegada: nome.value,
+    responsavel: responsavel.value
+  }
+  storage.add(newProduto).then(item => {
+    console.log(item)
+    itemFound.innerHTML = "Item added Successfully"
+  })
+})
+
 textInput.addEventListener('input', () => {
   const inputValue = textInput.value
   storage.find(inputValue).then(item => {
     itemFound.innerHTML = JSON.stringify(item, null, 2)
+  }).catch(err => {
+    console.log(err)
+    itemFound.innerHTML = ""
   })
 
-
 })
+
+
+
 
 /***/ }),
 /* 2 */
